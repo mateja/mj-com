236b27013f5c39cae705dbec889d470ceca28e43
Ian Kenney <ian@lostock.co.uk>
2013-02-02 00:01:59 +0000

    clean app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MjCom::Application.config.secret_token = 'cac10a4fe5479ff8bfffe83bc95464f95dd3b45aa38d031b91e443a98c2b2a20ca18a0200d2d824213a374454b38a4ee6224e86654b11234c8801ed7048c63d9'
